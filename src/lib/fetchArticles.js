eb6b7529c3c8fe4120b0429be048b0ebaaa83e3e
Tomislav Dukez <tomdu3@ymail.com>
2025-03-28 10:17:17 +0000

    add fetch collection from wix studio

---
+++ src/lib/fetchArticles.js
@@ -0,0 +1,16 @@
+import { createClient, OAuthStrategy } from '@wix/sdk';
+import { items } from '@wix/data';
+
+const WixClient = createClient({
+    modules: { items },
+    auth: new OAuthStrategy({
+        clientId: import.meta.env.WIX_CLIENT_ID,
+    }),
+});
+
+export default async function fetchArticles() {
+    let query = WixClient.items.query('Articles');
+    
+    const articles = await query.find()
+    return articles.items;
+}
